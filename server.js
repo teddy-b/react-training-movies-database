c90efe57e39f0895425db345d01f1af286102dd2
Teodora Boneva <teddyboneva@gmail.com>
2018-04-16 16:57:19 +0300

    Remove sendFile

--- server.js
+++ server.js
@@ -5,6 +5,4 @@ const app = express()
 
 app.use(express.static("./dist"))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, "./dist/index.html")))
-
 app.listen(3000, () => console.log('Movies Database app listening on port 3000!'))
