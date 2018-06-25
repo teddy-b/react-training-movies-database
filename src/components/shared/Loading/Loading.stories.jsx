2b0cbe902d828a44af7a6c779482d82053787aa4
Teodora Boneva <teddyboneva@gmail.com>
2018-06-25 13:15:46 +0300

    Add types to stories

--- src/components/shared/Loading/Loading.stories.jsx
+++ src/components/shared/Loading/Loading.stories.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+/* @flow */
+
+import * as React from 'react'
 
 import { storiesOf } from '@storybook/react'
+
+import type { Renderable } from '@storybook/react'
 
 import Loading from './Loading'
 
 storiesOf('Components/Loading', module)
-  .add('default', () => (
+  .add('default', (): Renderable => (
     <Loading />
   ))
